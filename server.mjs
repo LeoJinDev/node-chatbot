2f0a0324b8511ec4d954d042653fffde0c62962e
Leo Jin <leojin313@gmail.com>
2022-12-07 23:37:32 -0800

    create project

---
+++ server.mjs
@@ -0,0 +1,28 @@
+import {ChatGPTAPI} from 'chatgpt'
+import dotenv from 'dotenv-safe'
+import express from 'express'
+dotenv.config()
+
+const app = express()
+
+app.get('/', async (req, res) => {
+  const quiz = req.query.quiz
+  // sessionToken is required; see below for details
+  const api = new ChatGPTAPI({
+    sessionToken: process.env.SESSION_TOKEN
+  })
+
+  // ensure the API is properly authenticated
+  await api.ensureAuth()
+
+  // send a message and wait for the response
+  const response = await api.sendMessage(
+    quiz
+  )
+
+  res.status(200).send(response);
+})
+
+app.listen(3088, () => {
+  console.log(`Example app listening at http://localhost:3088`)
+})
